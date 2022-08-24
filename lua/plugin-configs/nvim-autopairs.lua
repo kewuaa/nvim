000a8233663b399a7f0ad58b78ce5be6193d9f33
kewuaa <1692525710@qq.com>
2022-08-24 08:13:07 +0800

    update

--- lua/plugin-configs/nvim-autopairs.lua
+++ lua/plugin-configs/nvim-autopairs.lua
@@ -2,8 +2,22 @@ local M = {}
 
 function M.config()
     require('nvim-autopairs').setup({
+        map_bs = false,
         map_c_h = true,
         map_c_w = true,
+        check_ts = true,
+        enable_check_bracket_line = false,
+        ignored_next_char = "[%w%.]",
+        fast_wrap = {
+            map = '<M-e>',
+            chars = { '{', '[', '(', '"', "'" },
+            pattern = [=[[%'%"%)%>%]%)%}%,]]=],
+            end_key = '$',
+            keys = 'qwertyuiopzxcvbnmasdfghjkl',
+            check_comma = true,
+            highlight = 'Search',
+            highlight_grey='Comment'
+        },
     })
 
     local cmp_autopairs = require('nvim-autopairs.completion.cmp')
