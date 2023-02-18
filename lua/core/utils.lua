da5f1f6c08a93f56b239059e709b15585d1eec24
kewuaa <1692525710@qq.com>
2023-02-18 08:32:21 +0800

    update get_cwd()

--- lua/core/utils.lua
+++ lua/core/utils.lua
@@ -46,14 +46,16 @@ end
 
 
 M.get_cwd = function ()
-    local activate_clients = vim.lsp.get_active_clients()
+    local activate_clients = vim.lsp.get_active_clients({
+        bufnr = 0
+    })
     local num = #activate_clients
     local root = nil
     if num > 0 then
         root = activate_clients[num].config.root_dir
     end
     if not root then
-        root = fn.getcwd()
+        root = fn.expand('%:p:h')
     end
     return root
 end
