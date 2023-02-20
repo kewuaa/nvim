6883e637861f0bf97e123ef9545beccceefbc2ec
kewuaa <1692525710@qq.com>
2023-02-20 22:29:31 +0800

    update config of neotree

--- lua/plugins/tools/configs.lua
+++ lua/plugins/tools/configs.lua
@@ -586,62 +586,15 @@ configs.neotree = function()
                 event = "file_renamed",
                 handler = function(args)
                     -- fix references to file
-                    vim.notify(args.source .. " renamed to " .. args.destination)
+                    print(args.source .. " renamed to " .. args.destination)
                 end
             },
             {
                 event = "file_moved",
                 handler = function(args)
                     -- fix references to file
-                    vim.notify(args.source .. " moved to " .. args.destination)
+                    print(args.source .. " moved to " .. args.destination)
                 end
-            },
-            {
-                event = "file_open_requested",
-                handler = function(args)
-                    local state = args.state
-                    local path = args.path
-                    local open_cmd = args.open_cmd or "edit"
-
-                    -- use last window if possible
-                    local suitable_window_found = false
-                    local nt = require("neo-tree")
-                    if nt.config.open_files_in_last_window then
-                        local prior_window = nt.get_prior_window()
-                        if prior_window > 0 then
-                            local success = pcall(vim.api.nvim_set_current_win, prior_window)
-                            if success then
-                                suitable_window_found = true
-                            end
-                        end
-                    end
-
-                    -- find a suitable window to open the file in
-                    if not suitable_window_found then
-                        if state.window.position == "right" then
-                            vim.cmd("wincmd t")
-                        else
-                            vim.cmd("wincmd w")
-                        end
-                    end
-                    local attempts = 0
-                    while attempts < 4 and vim.bo.filetype == "neo-tree" do
-                        attempts = attempts + 1
-                        vim.cmd("wincmd w")
-                    end
-                    if vim.bo.filetype == "neo-tree" then
-                        -- Neo-tree must be the only window, restore it's status as a sidebar
-                        local winid = vim.api.nvim_get_current_win()
-                        local width = require("neo-tree.utils").get_value(state, "window.width", 40)
-vim.cmd("vsplit " .. path)
-                        vim.api.nvim_win_set_width(winid, width)
-                    else
-                        vim.cmd(open_cmd .. " " .. path)
-                    end
-
-                    -- If you don't return this, it will proceed to open the file using built-in logic.
-                    return { handled = true }
-end
             },
         },
         source_selector = {
