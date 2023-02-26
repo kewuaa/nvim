b21188a25c0a2346b0ad75176ed81923eacabfb8
kewuaa <1692525710@qq.com>
2023-02-26 21:02:18 +0800

    delete commands

--- lua/lsp/clangd.lua
+++ lua/lsp/clangd.lua
@@ -11,31 +11,6 @@ vim.list_extend(rootmarks, {
     '.clang-format',
 })
 
-local function switch_source_header_splitcmd(bufnr, splitcmd)
-    local nvim_lsp = require("lspconfig")
-    bufnr = nvim_lsp.util.validate_bufnr(bufnr)
-    local clangd_client = nvim_lsp.util.get_active_client_by_name(bufnr, "clangd")
-    local params = { uri = vim.uri_from_bufnr(bufnr) }
-    if clangd_client then
-        clangd_client.request("textDocument/switchSourceHeader", params, function(err, result)
-        if err then
-                error(tostring(err))
-            end
-            if not result then
-                vim.notify("Corresponding file can’t be determined", vim.log.levels.ERROR, { title = "LSP Error!" })
-                return
-            end
-            vim.api.nvim_command(splitcmd .. " " .. vim.uri_to_fname(result))
-        end)
-    else
-        vim.notify(
-            "Method textDocument/switchSourceHeader is not supported by any active server on this buffer",
-            vim.log.levels.ERROR,
-            { title = "LSP Error!" }
-        )
-    end
-end
-
 clangd.rootmarks = rootmarks
 clangd.filetypes = {
     "c",
@@ -56,26 +31,6 @@ clangd.cmd = {
     "--completion-style=detailed",
     "--header-insertion-decorators",
     "--header-insertion=iwyu",
-}
-clangd.commands = {
-    ClangdSwitchSourceHeader = {
-        function()
-            switch_source_header_splitcmd(0, "edit")
-        end,
-        description = "Open source/header in current buffer",
-    },
-    ClangdSwitchSourceHeaderVSplit = {
-        function()
-            switch_source_header_splitcmd(0, "vsplit")
-        end,
-        description = "Open source/header in a new vsplit",
-    },
-    ClangdSwitchSourceHeaderSplit = {
-        function()
-            switch_source_header_splitcmd(0, "split")
-        end,
-        description = "Open source/header in a new split",
-    },
 }
 
 return clangd
