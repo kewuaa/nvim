1ec14acb9d37dc92df3a99e06fda5cb03be9e376
kewuaa <1692525710@qq.com>
2023-02-27 14:48:06 +0800

    add cwd option for dap

--- lua/plugins/tools/configs.lua
+++ lua/plugins/tools/configs.lua
@@ -225,6 +225,7 @@ configs.nvim_dap = function()
 
             -- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
 
+            cwd = require('core.utils').get_cwd,
             program = "${file}"; -- This configuration will launch the current file if used.
             pythonPath = function()
                 -- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
