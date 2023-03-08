d25a9cd0fb1d426c0326edc0d76c888b4dd5f5c4
kewuaa <1692525710@qq.com>
2023-03-08 21:40:37 +0800

    fix error config

--- lua/plugins/tools/configs.lua
+++ lua/plugins/tools/configs.lua
@@ -633,24 +633,14 @@ configs.nvim_window_picker = function()
     })
 end
 
-configs.nvim_window = function()
-    require('nvim-window').setup({
-        -- The characters available for hinting windows.
-        chars = {
-            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
-            'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+configs.windows = function ()
+    require('windows').setup({
+        ignore = {
+            filetype = require('core.settings').exclude_filetypes,
         },
-
-        -- A group to use for overwriting the Normal highlight group in the floating
-        -- window. This can be used to change the background color.
-        normal_hl = 'Normal',
-
-        -- The highlight group to apply to the line that contains the hint characters.
-        -- This is used to make them stand out more.
-        hint_hl = 'Bold',
-
-        -- The border style to use for the floating window.
-        border = 'single'
+        animation = {
+            enabled = false,
+        }
     })
 end
 
