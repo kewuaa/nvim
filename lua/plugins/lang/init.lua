33ab64b043bc778d046e161df96398642cb2a07b
kewuaa <1692525710@qq.com>
2023-04-11 21:23:19 +0800

    add nvim-lint to support cython-lint

--- lua/plugins/lang/init.lua
+++ lua/plugins/lang/init.lua
@@ -53,4 +53,25 @@ return {
             {'m-demare/hlargs.nvim', config = configs.hlargs},
         },
     },
+
+    -- lint system
+    {
+        'mfussenegger/nvim-lint',
+        lazy = true,
+        init = function()
+            vim.api.nvim_create_autocmd('FileType', {
+                pattern = 'pyrex',
+                once = true,
+                callback = function()
+                    vim.fn.timer_start(
+                        600,
+                        function()
+                            vim.api.nvim_command [[Lazy load nvim-lint]]
+                        end
+                    )
+                end
+            })
+        end,
+        config = configs.nvim_lint,
+    },
 }
