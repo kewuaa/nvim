7828fbb8e2aa6c12dd4d41eacb799d0d5696118c
kewuaa <1692525710@qq.com>
2023-04-11 13:40:38 +0800

    update for py3.11

--- python3/pyutils.py
+++ python3/pyutils.py
@@ -1,15 +1,15 @@
 from pathlib import Path
 
 try:
-    import rtoml as toml
+    import tomllib as toml
 except ImportError:
-    import tomli as toml
+    import rtoml as toml
 
 
 def pyread_toml(file: str) -> dict:
     file = Path(file)
     if not file.exists():
         raise RuntimeError(f'{file} not exists')
-    with open(file) as f:
+    with open(file, 'rb') as f:
         content = toml.load(f)
     return content
