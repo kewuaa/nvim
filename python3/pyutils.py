c0582e04d01a7511fc13877c6eee385db1bf30f8
kewuaa <1692525710@qq.com>
2023-04-12 14:51:47 +0800

    fix error because of different toml lib used

--- python3/pyutils.py
+++ python3/pyutils.py
@@ -1,15 +1,21 @@
 from pathlib import Path
 
 try:
-    import tomllib as toml
+    import tomllib
+    def read_toml(file: str) -> dict:
+        with open(file, 'rb') as f:
+            conten = tomllib.load(f)
+        return conten
 except ImportError:
-    import rtoml as toml
+    import rtoml
+    def read_toml(file: str) -> dict:
+        with open(file) as f:
+            content = rtoml.load(f)
+        return content
 
 
 def pyread_toml(file: str) -> dict:
     file = Path(file)
     if not file.exists():
         raise RuntimeError(f'{file} not exists')
-    with open(file, 'rb') as f:
-        content = toml.load(f)
-    return content
+    return read_toml(file)
