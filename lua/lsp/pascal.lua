49f88bb24b07235c563f528aa5462c06d684975d
kewuaa <1692525710@qq.com>
2023-07-05 13:50:51 +0800

    update rootmarks of pascal

--- lua/lsp/pascal.lua
+++ lua/lsp/pascal.lua
@@ -2,7 +2,7 @@ local M = {}
 local settings = require("core.settings")
 local rootmarks = settings.get_rootmarks()
 vim.list_extend(rootmarks, {
-    '*.lpi', '*.lps'
+    "makefile.fpc", "Makefile.fpc"
 })
 
 M.pasls = {
