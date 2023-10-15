cf0010e1a25fb4f6c9ff4cf425a656a668051915
kewuaa <1692525710@qq.com>
2023-10-16 07:34:09 +0800

    update config for rainbow-delimiters

--- lua/plugins/lang/configs.lua
+++ lua/plugins/lang/configs.lua
@@ -52,7 +52,7 @@ end
 
 configs.rainbow_delimiters = function()
     local rainbow_delimiters = require("rainbow-delimiters")
-    require("rainbow-delimiters.setup")({
+    require("rainbow-delimiters.setup").setup({
         strategy = {
             [''] = rainbow_delimiters.strategy['global'],
             vim = rainbow_delimiters.strategy['local'],
