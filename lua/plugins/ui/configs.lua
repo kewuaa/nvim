ff2b7e531d55f751b1d71853abe441fae78e6131
kewuaa <1692525710@qq.com>
2023-11-25 11:00:47 +0800

    remove useless config

--- lua/plugins/ui/configs.lua
+++ lua/plugins/ui/configs.lua
@@ -281,35 +281,4 @@ configs.neodim = function ()
     })
 end
 
-configs.colorful_winsep = function ()
-    local colorful_winsep = require('colorful-winsep')
-    local exclude_filetypes = require('core.settings').exclude_filetypes
-    colorful_winsep.setup({
-        -- highlight for Window separator
-        highlight = {
-            bg = '#303446',
-            fg = '#c6d0f5',
-        },
-        -- timer refresh rate
-        interval = 30,
-        -- This plugin will not be activated for filetype in the following table.
-        no_exec_files = vim.list_slice(exclude_filetypes, 1, #exclude_filetypes - 1),
-        -- Symbols for separator lines, the order: horizontal, vertical, top left, top right, bottom left, bottom right.
-        symbols = { "━", "┃", "┏", "┓", "┗", "┛" },
-        close_event = function()
-            -- Executed after closing the window separator
-        end,
-        create_event = function()
-            local win_n = require("colorful-winsep.utils").calculate_number_windows()
-            if win_n == 2 then
-                local win_id = vim.fn.win_getid(vim.fn.winnr('h'))
-                local filetype = vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(win_id), 'filetype')
-                if filetype == "neo-tree" then
-                    colorful_winsep.NvimSeparatorDel()
-                end
-            end
-        end,
-    })
-end
-
 return configs
