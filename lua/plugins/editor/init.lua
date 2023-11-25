23dfadbae9c6ca91e5fe9464a2ef63d247a7313e
kewuaa <1692525710@qq.com>
2023-11-25 09:55:36 +0800

    update config of `nvim-treesitter-textobjects`

--- lua/plugins/editor/init.lua
+++ lua/plugins/editor/init.lua
@@ -26,17 +26,12 @@ return {
         event = "VeryLazy",
         config = configs.mini_ai,
         dependencies = {
-            {
-                'nvim-treesitter/nvim-treesitter-textobjects',
-                init = function()
-                    -- disable rtp plugin, as we only need its queries for mini.ai
-                    -- In case other textobject modules are enabled, we will load them
-                    -- once nvim-treesitter is loaded
-                    require("lazy.core.loader").disable_rtp_plugin("nvim-treesitter-textobjects")
-                    -- load_textobjects = true
-                end
-            },
         }
+    },
+    {
+        'nvim-treesitter/nvim-treesitter-textobjects',
+        lazy = true,
+        config = configs.nvim_treesitter_textobjects,
     },
 
     -- 高亮相同单词
