ceb8f34d316f337c291000a655ddec23c4ecc765
kewuaa <1692525710@qq.com>
2024-01-05 07:47:06 +0800

    use schedule to avoid block

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -22,7 +22,9 @@ local languages = {
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
     if vim.list_contains({'pyright', 'cyright'}, client.name) then
-        require("core.utils.python").parse_pyenv(client.config.root_dir)
+        vim.schedule(function()
+            require("core.utils.python").parse_pyenv(client.config.root_dir)
+        end)
     end
     -- Enable completion triggered by <c-x><c-o>
     vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
