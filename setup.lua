976b0b682bd5fb647fbdb1fb36fd43f58c99c903
kewuaa <202321011111@std.uestc.edu.cn>
2024-01-18 09:07:53 +0800

    update setup.lua

--- setup.lua
+++ setup.lua
@@ -1,70 +1,68 @@
-local path = vim.fn.stdpath("config"):gsub("\\", "/")
 local settings = require("core.settings")
-local alacritty_config = ([[
-working_directory = "%s"
-
-import = ["%s"]
-]])
-local win_special = [[
-
-[shell]
-program = "cmd"
-args = ["/k clink inject"]
-]]
-local clink_config = ([[
-$include %s
-]]):format(path .. "/clink_inputrc")
-
-local function write_alacritty_config()
-    local dest_dir = nil
-    if settings.is_Windows then
-        alacritty_config = alacritty_config:format(
-            vim.fn.getenv("USERPROFILE"):gsub("\\", "/"),
-            path .. "/alacritty.toml"
-        )
-        alacritty_config = alacritty_config .. win_special
-        dest_dir = vim.fn.getenv("APPDATA") .. "/alacritty"
+local config_dir = vim.fn.stdpath("config")
+local home_dir = nil
+local clink_config_path = nil
+local alacritty_config_dir = nil
+local kitty_config_dir = nil
+if settings.is_Windows then
+    home_dir = os.getenv("USERPROFILE"):gsub("\\", "/")
+    alacritty_config_dir = os.getenv("APPDATA"):gsub("\\", "/") .. "/alacritty"
+    clink_config_path = home_dir .. "/.inputrc"
+    local file = nil
+    -- write clink config
+    file = io.open(clink_config_path, "w")
+    assert(file ~= nil)
+    file:write(("$include %s\n"):format(config_dir .. "/clink_inputrc"))
+    file:close()
+    vim.print(("clink config successfully write to `%s`"):format(clink_config_path))
+    -- write alacritty config
+    if vim.fn.isdirectory(alacritty_config_dir) == 0 then
+        vim.fn.mkdir(alacritty_config_dir, "pR")
+        vim.print(("directory `%s` not exists, create it"):format(alacritty_config_dir))
+    end
+    file = io.open(alacritty_config_dir .. "/alacritty.toml", "w")
+    assert(file ~= nil)
+    file:write(('working_directory = "%s"\n\n'):format(home_dir))
+    file:write(('import = ["%s"]\n\n'):format(config_dir .. "/alacritty.toml"))
+    file:write("[shell]\n")
+    file:write('program = "cmd"\n')
+    file:write('args = ["/k", "clink", "inject"]\n')
+    file:close()
+    vim.print(("alacritty config successfully write to `%s`"):format(alacritty_config_dir))
+    -- build python extension
+    local output = vim.fn.system("python setup.py build_ext -i")
+    vim.print(output)
+else
+    home_dir = os.getenv("HOME")
+    alacritty_config_dir = home_dir .. "/.config/alacritty"
+    kitty_config_dir = home_dir .. "/.config/kitty"
+    local file = nil
+    vim.print("alacritty or kitty:")
+    local terminal = io.read()
+    if terminal == "alacritty" then
+        -- write alacritty config
+        if vim.fn.isdirectory(alacritty_config_dir) == 0 then
+            vim.fn.mkdir(alacritty_config_dir, "pR")
+            vim.print(("directory `%s` not exists, create it"):format(alacritty_config_dir))
+        end
+        file = io.open(alacritty_config_dir .. "/alacritty.toml", "w")
+        assert(file ~= nil)
+        file:write(('working_directory = "%s"\n\n'):format(home_dir))
+        file:write(('import = ["%s"]\n'):format(config_dir .. "/alacritty.toml"))
+        file:close()
+        vim.print(("alacritty config successfully write to `%s`"):format(alacritty_config_dir))
+    elseif terminal == "kitty" then
+        -- write kitty config
+        if vim.fn.isdirectory(kitty_config_dir) == 0 then
+            vim.fn.mkdir(kitty_config_dir, "pR")
+            vim.print(("directory `%s` not exists, create it"):format(kitty_config_dir))
+        end
+        file = io.open(kitty_config_dir .. "/kitty.conf", "w")
+        assert(file ~= nil)
+        file:write(('include %s\n'):format(config_dir .. "/kitty.conf"))
+        file:close()
+        vim.print(("kitty config successfully write to `%s`"):format(kitty_config_dir))
     else
-        local home_dir = vim.fn.getenv("HOME")
-        alacritty_config = alacritty_config:format(
-            home_dir,
-            path .. "/alacritty.toml"
-        )
-        dest_dir = home_dir .. "/.config/alacritty"
-    end
-    assert(vim.fn.exists("*mkdir") == 1)
-    if vim.fn.isdirectory(dest_dir) == 0 then
-        vim.fn.mkdir(dest_dir, "pR")
-        vim.print(("directory `%s` not exists, create it"):format(dest_dir))
-    end
-    local dest_path = dest_dir .. "/alacritty.toml"
-    local file = io.open(dest_path, "w")
-    assert(file ~= nil)
-    file:write(alacritty_config)
-    file:close()
-    vim.print(("alacritty config successfully write to `%s`"):format(dest_path))
-end
-
-local function write_clink_config()
-    local home_dir = vim.fn.getenv("HOME")
-    assert(home_dir ~= nil)
-    local dest_path = home_dir .. '/.inputrc'
-    local file = io.open(dest_path, "w")
-    assert(file ~= nil)
-    file:write(clink_config)
-    file:close()
-    vim.print(("clink config successfully write to `%s`"):format(dest_path))
-end
-
-local function setup()
-    if not settings.is_wsl then
-        write_alacritty_config()
-    end
-    if settings.is_Windows then
-        write_clink_config()
-        local output = vim.fn.system("python setup.py build_ext -i")
-        vim.print(output)
+        vim.print("invalid input")
     end
 end
-
-setup()
