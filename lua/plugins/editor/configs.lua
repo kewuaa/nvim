3a24593fee1785e241cc48027141be67f94dda50
kewuaa <202321011111@std.uestc.edu.cn>
2024-01-30 09:28:28 +0800

    add backspace map for <c-h> and <c-w> and <c-u>

--- lua/plugins/editor/configs.lua
+++ lua/plugins/editor/configs.lua
@@ -52,6 +52,13 @@ configs.mini_pairs = function()
         -- In which modes mappings from this `config` should be created
         modes = { insert = true, command = true, terminal = true },
     })
+    local map_bs = function(lhs, rhs)
+        vim.keymap.set('i', lhs, rhs, { expr = true, replace_keycodes = false })
+    end
+
+    map_bs('<C-h>', 'v:lua.MiniPairs.bs()')
+    map_bs('<C-w>', 'v:lua.MiniPairs.bs("\23")')
+    map_bs('<C-u>', 'v:lua.MiniPairs.bs("\21")')
 end
 
 configs.mini_comment = function()
