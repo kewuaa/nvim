d91faa17842e94b9fb910b6f332204f9de92190a
kewuaa <202321011111@std.uestc.edu.cn>
2024-03-28 07:13:33 +0800

    do not disable focus.nvim in filetype 'help'

--- lua/plugins/tools/configs.lua
+++ lua/plugins/tools/configs.lua
@@ -559,27 +559,26 @@ configs.focus = function()
     vim.api.nvim_create_autocmd('WinEnter', {
         group = augroup,
         callback = function(_)
-            if vim.tbl_contains(ignore_buftypes, vim.bo.buftype)
-                then
-                    vim.w.focus_disable = true
-                else
-                    vim.w.focus_disable = false
-                end
-            end,
-            desc = 'Disable focus autoresize for BufType',
+            if vim.tbl_contains(ignore_buftypes, vim.bo.buftype) then
+                vim.w.focus_disable = true
+            else
+                vim.w.focus_disable = false
+            end
+        end,
+        desc = 'Disable focus autoresize for BufType',
         })
 
-        vim.api.nvim_create_autocmd('FileType', {
-            group = augroup,
-            callback = function(_)
-                if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
-                    vim.b.focus_disable = true
-                else
-                    vim.b.focus_disable = false
-                end
-            end,
-            desc = 'Disable focus autoresize for FileType',
-        })
+    vim.api.nvim_create_autocmd('FileType', {
+        group = augroup,
+        callback = function(_)
+            if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) and vim.bo.filetype ~= "help" then
+                vim.b.focus_disable = true
+            else
+                vim.b.focus_disable = false
+            end
+        end,
+        desc = 'Disable focus autoresize for FileType',
+    })
     require('focus').setup({
         ui = {
             number = false, -- Display line numbers in the focussed window only
