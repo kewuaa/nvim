bfa36ffd6fbe01f0a7447c73ecf0048894435c23
kewuaa <202321011111@std.uestc.edu.cn>
2024-03-30 20:45:47 +0800

    ignore zig because treesitter-zig too slow

--- lua/plugins/lang/configs.lua
+++ lua/plugins/lang/configs.lua
@@ -4,6 +4,7 @@ local configs = {}
 configs.nvim_treesitter = function()
     require('nvim-treesitter.configs').setup({
         auto_install = true,
+        ignore_install = {"zig"},
         highlight = {
             enable = true,
             additional_vim_regex_highlighting = { "c", "cpp" },
