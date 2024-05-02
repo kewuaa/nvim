f05f1667bc994e744ee780cbc3d05977286336de
kewuaa <202321011111@std.uestc.edu.cn>
2024-05-02 11:14:37 +0800

    delete useless config for pylsp

--- lua/lsp/python.lua
+++ lua/lsp/python.lua
@@ -1,15 +1,6 @@
 local M = {}
 local settings = require('core.settings')
 local rootmarks = settings.get_rootmarks('pyproject.toml')
-local exclude = {
-    '.git',
-    '**/__pycache__',
-    'build',
-    'dist',
-    '.venv',
-    '.pytest_cache',
-    '.mypy_cache'
-}
 
 M.basedpyright = {
     rootmarks = rootmarks,
@@ -38,62 +29,5 @@ M.ruff = {
         client.server_capabilities.hoverProvider = false
     end,
 }
--- M.pylsp = {
---     rootmarks = rootmarks,
---     filetypes = {"python"},
---     cmd = {'pylsp'},
---     settings = {
---         pylsp = {
---             configurationSources = {},
---             plugins = {
---                 pyflakes = {enabled = false},
---                 pycodestyle = {enabled = false},
---                 autopep8 = {enabled = false},
---                 yapf = {enabled = false},
---                 flake8 = {enabled = false},
---                 mccabe = {enabled = false},
---                 ruff = {
---                     enabled = false,
---                     exclude = vim.list_extend({}, exclude),
---                     ignore = {'E402'},
---                     lineLength = 80,
---                 },
---                 pylsp_mypy = {
---                     enabled = false,
---                     exclude = vim.list_extend({}, exclude),
---                     overrides = {}
---                 },
---                 jedi = {
---                     -- environment = '',
---                     -- extra_paths = {},
---                     auto_import_modules = {'numpy', 'pandas'}
---                 },
---                 jedi_completion = {
---                     include_params = true,
---                     resolve_at_most = 15,
---                     fuzzy = true,
---                     cache_for = {
---                         'numpy',
---                         'pandas',
---                         'sklearn',
---                         'seaborn',
---                         'matplotlib',
---                     },
---                 },
---                 preload = {
---                     enabled = false,
---                     modules = {},
---                 }
---             },
---         },
---     },
---     on_new_config = function(new_config, new_root)
---         local pyutils = require("core.utils.python")
---         pyutils.parse_pyenv(new_root)
---         local venv = pyutils.get_current_env().path
---         vim.list_extend(new_config.settings.pylsp.plugins.pylsp_mypy.overrides, {"--python-executable", venv, true})
---         new_config.settings.pylsp.plugins.jedi.environment = vim.fn.fnamemodify(venv, ':h:h')
---     end
--- }
 
 return M
