76a9f3b83223c800a75e319a89d5555b7fd291bb
kewuaa <202321011111@std.uestc.edu.cn>
2024-05-25 19:34:31 +0800

    update

--- lua/core/utils/im.lua
+++ lua/core/utils/im.lua
@@ -11,42 +11,42 @@ local function switch_to_zh()
 end
 
 function M.toggle_imtoggle(opts)
-     if opts == nil then
-         opts = {
-             slient = false,
-         }
-     end
-     local enabled = opts.enabled or not imtoogle_enabled
-     local im_switch_to_en = nil
-     local im_switch_to_zh = nil
-     if settings.is_Windows then
-         im_switch_to_en = vim.fn.IMSwitchToEN
-         im_switch_to_zh = vim.fn.IMSwitchToZH
-     else
-         im_switch_to_en = switch_to_en
-         im_switch_to_zh = switch_to_zh
-     end
-     if enabled then
-         if not opts.silent then
-             vim.notify('imtoggle enabled')
-         end
-         vim.api.nvim_create_augroup('IM', {clear = true})
-         vim.api.nvim_create_autocmd('InsertEnter', {
-             group = 'IM',
-             callback = im_switch_to_zh
-         })
-         vim.api.nvim_create_autocmd('InsertLeave', {
-             group = 'IM',
-             callback = im_switch_to_en
-         })
-         imtoogle_enabled = true
-     else
-         if not opts.silent then
-             vim.notify('imtoggle disabled')
-         end
-         vim.api.nvim_del_augroup_by_name('IM')
-         imtoogle_enabled = false
-     end
+    if opts == nil then
+        opts = {
+            slient = false,
+        }
+    end
+    local enabled = opts.enabled or not imtoogle_enabled
+    local im_switch_to_en = nil
+    local im_switch_to_zh = nil
+    if settings.is_Windows then
+        im_switch_to_en = vim.fn.IMSwitchToEN
+        im_switch_to_zh = vim.fn.IMSwitchToZH
+    else
+        im_switch_to_en = switch_to_en
+        im_switch_to_zh = switch_to_zh
+    end
+    if enabled then
+        if not opts.silent then
+            vim.notify('imtoggle enabled')
+        end
+        local im = vim.api.nvim_create_augroup('_IM_', {clear = true})
+        vim.api.nvim_create_autocmd('InsertEnter', {
+            group = im,
+            callback = im_switch_to_zh
+        })
+        vim.api.nvim_create_autocmd('InsertLeave', {
+            group = im,
+            callback = im_switch_to_en
+        })
+        imtoogle_enabled = true
+    else
+        if not opts.silent then
+            vim.notify('imtoggle disabled')
+        end
+        vim.api.nvim_del_augroup_by_name('IM')
+        imtoogle_enabled = false
+    end
 end
 
 M.init = function()
