9bea69972e1612f3804144d55e1fdad9eb47c26c
kewuaa <202321011111@std.uestc.edu.cn>
2024-05-27 14:51:50 +0800

    fix error of native snippets

--- lua/plugins/completion/configs.lua
+++ lua/plugins/completion/configs.lua
@@ -59,7 +59,16 @@ configs.nvim_cmp = function()
         preselect = cmp.PreselectMode.None,
         snippet = {
             expand = function(args)
+                -- Native sessions don't support nested snippet sessions.
+                -- Always use the top-level session.
+                -- Otherwise, when on the first placeholder and selecting a new completion,
+                -- the nested session will be used instead of the top-level session.
+                local session = vim.snippet.active() and vim.snippet._session or nil
                 vim.snippet.expand(args.body)
+                -- Restore top-level session when needed
+                if session then
+                    vim.snippet._session = session
+                end
             end,
         },
         sorting = {
