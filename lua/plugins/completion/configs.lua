99b792e7580d2a8c410ad9e2dbc7301f59987e30
kewuaa <202321011111@std.uestc.edu.cn>
2024-05-27 17:21:04 +0800

    only when the char before cusor is number or word return true

--- lua/plugins/completion/configs.lua
+++ lua/plugins/completion/configs.lua
@@ -5,7 +5,10 @@ configs.nvim_cmp = function()
     local compare = cmp.config.compare
     local has_words_before = function()
         local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+        return col ~= 0
+            and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]
+                :sub(col, col)
+                :match("%w") ~= nil
     end
     local cmp_window = require("cmp.utils.window")
     cmp_window.info_ = cmp_window.info
