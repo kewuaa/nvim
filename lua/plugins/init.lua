763434b3c02bfdf4b806389b194fded8181ba00e
kewuaa <202321011111@std.uestc.edu.cn>
2024-05-28 10:58:33 +0800

    small change

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,7 +2,7 @@ local plugins = {}
 local plugins_dir = vim.fn.stdpath('config') .. '/lua/plugins'
 
 for _, plugin in ipairs(
-    vim.fn.globpath(plugins_dir, '*/init.lua', 0, 1)
+    vim.fn.globpath(plugins_dir, '*/init.lua', false, 1)
 ) do
     plugin = string.match(plugin, '[/\\]([^/\\]-)[/\\]init.lua')
     vim.list_extend(
