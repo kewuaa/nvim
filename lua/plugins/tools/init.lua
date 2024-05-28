11f68d90dc56456d28b0b0a15687df6775c5b018
kewuaa <202321011111@std.uestc.edu.cn>
2024-05-28 09:22:36 +0800

    delete unnecessary dependencies

--- lua/plugins/tools/init.lua
+++ lua/plugins/tools/init.lua
@@ -148,13 +148,7 @@ return {
         config = configs.leetcode,
         dependencies = {
             "nvim-telescope/telescope.nvim",
-            "nvim-lua/plenary.nvim", -- required by telescope
             "MunifTanjim/nui.nvim",
-
-            -- optional
-            "nvim-treesitter/nvim-treesitter",
-            "rcarriga/nvim-notify",
-            "nvim-tree/nvim-web-devicons",
         },
     },
 
