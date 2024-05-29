3d62fee9ebd71e2cb33bd23b8e2c060a0fee923a
kewuaa <202321011111@std.uestc.edu.cn>
2024-05-30 07:35:35 +0800

    use filetype instead of bufread

--- lua/plugins/editor/configs.lua
+++ lua/plugins/editor/configs.lua
@@ -47,16 +47,17 @@ configs.mini_cursorword = function()
     })
     local exclude_fts = require("core.settings").exclude_filetypes
     local mini_cursorword_group = vim.api.nvim_create_augroup("mini_cursorword", {clear = true})
-    vim.api.nvim_create_autocmd("BufRead", {
+    local disable = function()
+        local ft = vim.bo.filetype
+        if vim.tbl_contains(exclude_fts, ft) then
+            vim.b.minicursorword_disable = true
+        end
+    end
+    vim.schedule(disable)
+    vim.api.nvim_create_autocmd("FileType", {
         desc = "disable MiniursorWord on some filetypes",
         group = mini_cursorword_group,
-        callback = function()
-            local ft = vim.bo.filetype
-            local buftype = vim.bo.buftype
-            if buftype ~= "" or vim.tbl_contains(exclude_fts, ft) then
-                vim.b.minicursorword_disable = true
-            end
-        end
+        callback = disable
     })
 end
 
