6de3cab066c9fd316dba33ec6ea450d2b9ab7d43
kewuaa <202321011111@std.uestc.edu.cn>
2024-05-29 20:08:36 +0800

    add keymap to zoom window

--- lua/core/utils/init.lua
+++ lua/core/utils/init.lua
@@ -1,5 +1,30 @@
 local M = {}
-local fn = vim.fn
+local zoom_wind = nil
+local api, fn = vim.api, vim.fn
+
+local zoom = function()
+    if zoom_wind and vim.api.nvim_win_is_valid(zoom_wind) then
+        api.nvim_win_close(zoom_wind, true)
+        zoom_wind = nil
+        return
+    end
+    local bufnr = api.nvim_get_current_buf()
+    local term = api.nvim_list_uis()[1]
+    local width, height = term.width, term.height
+    local float_width = math.floor(width * 0.9)
+    local float_height = math.floor(height * 0.9)
+    local row = math.floor((height - float_height) / 2)
+    local col = math.floor((width - float_width) / 2)
+    zoom_wind = api.nvim_open_win(bufnr, true, {
+        relative = "editor",
+        row = row,
+        col = col,
+        width = float_width,
+        height = float_height,
+        border = "single"
+    })
+    api.nvim_command("normal! zz")
+end
 
 M.get_cwd = function ()
     local activate_clients = vim.lsp.get_clients({
@@ -26,6 +51,8 @@ M.get_bufsize = function(bufnr)
 end
 
 M.init = function()
+    vim.keymap.set("n", "<C-w>z", zoom, {silent = true, noremap = true})
+
     require('core.utils.bigfile').init()
     require('core.utils.im').init()
     require('core.utils.lsp').init()
