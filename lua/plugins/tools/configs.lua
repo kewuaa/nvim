2d68de2bfc123517557e07724855172199a4e7b0
kewuaa <202321011111@std.uestc.edu.cn>
2024-06-04 18:36:04 +0800

    fix error in config of dap

--- lua/plugins/tools/configs.lua
+++ lua/plugins/tools/configs.lua
@@ -232,30 +232,28 @@ configs.nvim_dap = function()
         dapui.close()
     end
 
-    local api = vim.api
     local keymap_restore = {}
     dap.listeners.after['event_initialized']['me'] = function()
-        for _, buf in pairs(api.nvim_list_bufs()) do
-            local keymaps = api.nvim_buf_get_keymap(buf, 'n')
+        for _, buf in pairs(vim.api.nvim_list_bufs()) do
+            local keymaps = vim.api.nvim_buf_get_keymap(buf, 'n')
             for _, keymap in pairs(keymaps) do
                 if keymap.lhs == "K" then
                     table.insert(keymap_restore, keymap)
-                    api.nvim_buf_del_keymap(buf, 'n', 'K')
+                    vim.api.nvim_buf_del_keymap(buf, 'n', 'K')
                 end
             end
         end
-        api.nvim_set_keymap(
+        vim.api.nvim_set_keymap(
             'n', 'K', '<Cmd>lua require("dap.ui.widgets").hover()<CR>', { silent = true })
     end
 
     dap.listeners.after['event_terminated']['me'] = function()
         for _, keymap in pairs(keymap_restore) do
-            api.nvim_buf_set_keymap(
-                keymap.buffer,
+            vim.keymap.set(
                 keymap.mode,
                 keymap.lhs,
-                keymap.rhs,
-                { silent = keymap.silent == 1 }
+                keymap.rhs or keymap.callback,
+                { silent = keymap.silent == 1, buffer = keymap.buffer }
             )
         end
         keymap_restore = {}
