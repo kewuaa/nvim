de13bb17167550369e4484fa4a9d2bb7c6dd7bde
kewuaa <202321011111@std.uestc.edu.cn>
2024-06-06 18:25:22 +0800

    update sign of gitsigns

--- lua/plugins/tools/configs.lua
+++ lua/plugins/tools/configs.lua
@@ -295,12 +295,12 @@ end
 configs.gitsigns = function()
     require('gitsigns').setup {
         signs = {
-            add = { text = "▒" },
-            change = { text = "▒" },
+            add = { text = "▎" },
+            change = { text = "▎" },
             delete = { text = "" },
             topdelete = { text = "" },
-            changedelete = { text = "▒" },
-            untracked = { text = "▒" },
+            changedelete = { text = "▎" },
+            untracked = { text = "▎" },
         },
         on_attach = function(buffer)
             local gitsigns = package.loaded.gitsigns
