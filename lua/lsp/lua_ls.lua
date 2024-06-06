35e79a7e39960587714902d365935daab39d5579
kewuaa <202321011111@std.uestc.edu.cn>
2024-06-06 15:55:15 +0800

    simplify

--- lua/lsp/lua_ls.lua
+++ lua/lsp/lua_ls.lua
@@ -1,29 +1,6 @@
 local M = {}
-
-local library = function()
-    local ret = {}
-    local version = vim.version().prerelease and "nightly" or "stable"
-    local f = debug.getinfo(1, "S").source:sub(2)
-    local types = vim.uv.fs_realpath(vim.fn.fnamemodify(f, ":h:h:h") .. "/types/" .. version)
-    table.insert(ret, types)
-
-    local function add(lib, filter)
-        ---@diagnostic disable-next-line: param-type-mismatch
-        for _, p in ipairs(vim.fn.expand(lib .. "/lua", false, true)) do
-            local plugin_name = vim.fn.fnamemodify(p, ":h:t")
-            p = vim.uv.fs_realpath(p)
-            if p and (not filter or filter[plugin_name]) then
-                table.insert(ret, p)
-            end
-        end
-    end
-    add("$VIMRUNTIME")
-    add(vim.fn.stdpath("config"))
-    -- local site = vim.fn.stdpath("data") .. "/site"
-    -- add(site .. "/pack/*/opt/*")
-    -- add(site .. "/pack/*/start/*")
-    return ret
-end
+local runtime_path = vim.fn.split(package.path, ";")
+vim.list_extend(runtime_path, {"?.lua", "?/init.lua"})
 
 M.lua_ls = {
     settings = {
@@ -37,7 +14,10 @@ M.lua_ls = {
                 keywordSnippet = "Disable"
             },
             workspace = {
-                library = library()
+                library = {
+                    vim.fn.expand("$VIMRUNTIME/lua"),
+                    vim.fn.stdpath("config") .. "/lua"
+                }
             },
             diagnostics = {
                 globals = {
