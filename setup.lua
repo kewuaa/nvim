07f4382ed05f851dbcd1e66eb3d5050fb30ff7ed
kewuaa <202321011111@std.uestc.edu.cn>
2024-07-13 11:21:35 +0800

    do not write if it exists

--- setup.lua
+++ setup.lua
@@ -2,7 +2,11 @@ local utils = require("utils")
 
 ---@param path string: path of the file to write
 ---@param lines string[]: lines to write
-local write = function(path, lines)
+local try_write = function(path, lines)
+    if vim.loop.fs_stat(path) then
+        vim.print(("%s exists, skipped"):format(path))
+        return
+    end
     local file
     file = io.open(path, "w")
     assert(file ~= nil)
@@ -18,7 +22,7 @@ if utils.is_win then
     local local_appdata_dir = os.getenv("LOCALAPPDATA"):gsub("\\", "/")
 
     local clink_config_path = home_dir .. "/.inputrc"
-    write(
+    try_write(
         clink_config_path,
         {
             ("$include %s"):format(config_dir .. "/clink_inputrc")
@@ -29,7 +33,7 @@ if utils.is_win then
         vim.fn.mkdir(clink_profile_path, "pR")
         vim.print(("directory `%s` not exists, create it"):format(clink_profile_path))
     end
-    write(
+    try_write(
         clink_profile_path .. "/clink_start.cmd",
         {
             "@echo off",
@@ -42,7 +46,7 @@ if utils.is_win then
         vim.fn.mkdir(alacritty_config_dir, "pR")
         vim.print(("directory `%s` not exists, create it"):format(alacritty_config_dir))
     end
-    write(
+    try_write(
         alacritty_config_dir .. "/alacritty.toml",
         {
             ('working_directory = "%s"'):format(home_dir),
