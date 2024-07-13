a807a422971812f78ccb5c040f1b62df34d71a0e
kewuaa <202321011111@std.uestc.edu.cn>
2024-07-13 10:52:14 +0800

    update setup.lua

--- setup.lua
+++ setup.lua
@@ -1,33 +1,51 @@
 local utils = require("utils")
-local config_dir = vim.fn.stdpath("config")
-local home_dir = nil
-local clink_config_path = nil
-local alacritty_config_dir = nil
-if utils.is_win then
-    config_dir = config_dir:gsub("\\", "/")
-    home_dir = os.getenv("USERPROFILE"):gsub("\\", "/")
-    alacritty_config_dir = os.getenv("APPDATA"):gsub("\\", "/") .. "/alacritty"
-    clink_config_path = home_dir .. "/.inputrc"
-    local file = nil
-    -- write clink config
-    file = io.open(clink_config_path, "w")
+
+---@param path string: path of the file to write
+---@param lines string[]: lines to write
+local write = function(path, lines)
+    local file
+    file = io.open(path, "w")
     assert(file ~= nil)
-    file:write(("$include %s\n"):format(config_dir .. "/clink_inputrc"))
+    file:write(vim.fn.join(lines, "\n"))
     file:close()
-    vim.print(("clink config successfully write to `%s`"):format(clink_config_path))
-    -- write alacritty config
+    vim.print(("%s successfully writed"):format(path))
+end
+
+if utils.is_win then
+    local config_dir = vim.fn.stdpath("config"):gsub("\\", "/")
+    local home_dir = os.getenv("USERPROFILE"):gsub("\\", "/")
+    local appdata_dir = os.getenv("APPDATA"):gsub("\\", "/")
+
+    local clink_config_path = home_dir .. "/.inputrc"
+    write(
+        clink_config_path,
+        {
+            ("$include %s"):format(config_dir .. "/clink_inputrc")
+        }
+    )
+    local clink_profile_path = appdata_dir .. "/clink"
+    write(
+        clink_profile_path .. "/clink_start.cmd",
+        {
+            "@echo off",
+            "%PYVENV%/default/Scripts/activate",
+        }
+    )
+
+    local alacritty_config_dir = appdata_dir .. "/alacritty"
     if vim.fn.isdirectory(alacritty_config_dir) == 0 then
         vim.fn.mkdir(alacritty_config_dir, "pR")
         vim.print(("directory `%s` not exists, create it"):format(alacritty_config_dir))
     end
-    file = io.open(alacritty_config_dir .. "/alacritty.toml", "w")
-    assert(file ~= nil)
-    file:write(('working_directory = "%s"\n\n'):format(home_dir))
-    file:write("[shell]\n")
-    file:write('program = "cmd"\n')
-    file:write('args = ["/k", "clink", "inject"]\n')
-    file:close()
-    vim.print(("alacritty config successfully write to `%s`"):format(alacritty_config_dir))
+    write(
+        alacritty_config_dir .. "/alacritty.toml",
+        {
+            ('working_directory = "%s"'):format(home_dir),
+            '[shell]',
+            'program = "cmd"',
+            'args = ["/k", "clink", "inject"]'
+        }
+    )
     -- build python extension
     local output = vim.fn.system("python setup.py build_ext -i")
     vim.print(output)
