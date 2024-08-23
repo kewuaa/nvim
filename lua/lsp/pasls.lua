551e5d2bf7b3c87ca7167abd102a7893b25c09a6
kewuaa <202321011111@std.uestc.edu.cn>
2024-08-23 21:36:53 +0800

    update pasls config

--- lua/lsp/pasls.lua
+++ lua/lsp/pasls.lua
@@ -1,4 +1,29 @@
 local M = {}
+local configs = require("lspconfig/configs")
+local util = require("lspconfig/util")
+
+configs.pascal = {
+  default_config = {
+    cmd = {
+      "pasls",
+      -- Uncomment for debugging:
+      --"--save-log", "pasls-log", "--save-replay", "pasls-replay"
+    };
+    filetypes = {"pascal"};
+    root_dir = util.root_pattern(".git", "Makefile.fpc");
+    init_options = {}
+  };
+  docs = {
+    description = [[
+https://github.com/Isopod/pascal-language-server
+
+`pascal-language-server`, a language server for Pascal, based on fpc.
+]];
+    default_config = {
+      root_dir = [[root_pattern(".git", "Makefile.fpc")]];
+    };
+  };
+};
 
 -- environment variables
 -- PP — Path to the FPC compiler executable
@@ -8,32 +33,6 @@ local M = {}
 -- FPCTARGETCPU — Target architecture (e.g. x86_64, AARCH64, ...)
 M.pasls = {
     rootmarks = {".git", "xmake.lua", "*.lpi"},
-    init_options = {
-        fpcOptions = {
-            "-Fu$(root)/unit",
-        },
-        -- procedure completions with parameters are inserted as snippets
-        insertCompletionsAsSnippets = true,
-        -- procedure completions with parameters (non-snippet) insert
-        -- empty brackets (and insert as snippet)
-        insertCompletionProcedureBrackets = true,
-        -- workspaces folders will be added to unit paths (i.e. -Fu)
-        includeWorkspaceFoldersAsUnitPaths = true,
-        -- workspaces folders will be added to include paths (i.e. -Fi)
-        includeWorkspaceFoldersAsIncludePaths = true,
-        -- syntax will be checked when file opens or saves
-        checkSyntax = true,
-        -- syntax errors will be published as diagnostics
-        publishDiagnostics = true,
-        -- enable workspace symbols
-        workspaceSymbols = true,
-        -- enable document symbols
-        documentSymbols = true,
-        -- completions contain a minimal amount of extra information
-        minimalisticCompletions = true,
-        -- syntax errors as shown in the UI with ‘window/showMessage’
-        showSyntaxErrors = true,
-    }
 }
 
 return M
