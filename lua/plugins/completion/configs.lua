c33d88fa8a37168b954fc4282396fc275bc4db7a
kewuaa <202321011111@std.uestc.edu.cn>
2024-12-11 21:23:19 +0800

    remove `fittencode.nvim`

--- lua/plugins/completion/configs.lua
+++ lua/plugins/completion/configs.lua
@@ -125,39 +125,4 @@ configs.copilot = function()
     })
 end
 
-configs.fittencode = function()
-    require("fittencode").setup({
-        disable_specific_inline_completion = {
-            suffixes = {}
-        },
-        inline_completion = {
-            enable = true,
-            auto_triggering_completion = false,
-        },
-        source_complection = {
-            enable = false
-        },
-        keymaps = {
-            inline = {
-                ['<TAB>'] = 'accept_all_suggestions',
-                ['<C-Down>'] = 'accept_line',
-                ['<C-Right>'] = 'accept_word',
-                ['<C-Up>'] = 'revoke_line',
-                ['<C-Left>'] = 'revoke_word',
-                ['<A-\\>'] = 'triggering_completion',
-            },
-            chat = {
-                ['q'] = 'close',
-                ['[c'] = 'goto_previous_conversation',
-                [']c'] = 'goto_next_conversation',
-                ['c'] = 'copy_conversation',
-                ['C'] = 'copy_all_conversations',
-                ['d'] = 'delete_conversation',
-                ['D'] = 'delete_all_conversations',
-            }
-        },
-        completion_mode = "inline"
-    })
-end
-
 return configs
