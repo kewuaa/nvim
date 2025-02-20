86e1696722f0710a3105e3350cd4d6026f4e1b12
kewuaa <202321011111@std.uestc.edu.cn>
2025-02-20 19:24:35 +0800

    automaticly pin to main.typ if exists

--- lua/lsp/tinymist.lua
+++ lua/lsp/tinymist.lua
@@ -1,5 +1,31 @@
 local M = {}
 
+local group = vim.api.nvim_create_augroup("tinymist_pin_main", {clear = true})
+vim.api.nvim_create_autocmd("LspAttach", {
+    group = group,
+    callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        assert(client)
+        if
+            client.name ~= "tinymist"
+            or not client.root_dir
+        then
+            return
+        end
+        local entry = vim.fs.joinpath(client.root_dir, "main.typ")
+        local ok, _ = vim.uv.fs_stat(entry)
+        if ok then
+            client:exec_cmd(
+                {
+                    title = "pin_main",
+                    command = "tinymist.pinMain",
+                    arguments = {entry},
+                }
+            )
+        end
+    end
+})
+
 M.tinymist = {
     rootmarks = {"typst.toml", ".git"},
     single_file_support = true
