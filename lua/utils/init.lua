8b52f4921a4fd2f4e7e33538538f9e4e7e703d58
kewuaa <202321011111@std.uestc.edu.cn>
2025-03-01 11:00:18 +0800

    update `get_cwd`

--- lua/utils/init.lua
+++ lua/utils/init.lua
@@ -28,10 +28,12 @@ M.get_cwd = function()
     local activate_clients = vim.lsp.get_clients({
         bufnr = 0
     })
-    local num = #activate_clients
     local root = nil
-    if num > 0 then
-        root = activate_clients[num].config.root_dir
+    for _, client in ipairs(activate_clients) do
+       root = client.config.root_dir
+       if root then
+           break
+       end
     end
     if not root or root == "" then
         local startpath = fn.expand('%:p:h')
