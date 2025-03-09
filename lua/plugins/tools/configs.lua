52619abd64486d7c03cc7c8f699898eea4e1f731
kewuaa <202321011111@std.uestc.edu.cn>
2025-03-09 22:32:09 +0800

    add example config

--- lua/plugins/tools/configs.lua
+++ lua/plugins/tools/configs.lua
@@ -369,6 +369,34 @@ configs.mini_files = function()
             map_split(buf_id, '<C-v>', 'belowright vertical')
         end,
     })
+
+    -- Set focused directory as current working directory
+    local set_cwd = function()
+        local path = (mini_files.get_fs_entry() or {}).path
+        if path == nil then return vim.notify('Cursor is not on valid entry') end
+        vim.fn.chdir(vim.fs.dirname(path))
+    end
+
+    -- Yank in register full path of entry under cursor
+    local yank_path = function()
+        local path = (mini_files.get_fs_entry() or {}).path
+        if path == nil then return vim.notify('Cursor is not on valid entry') end
+        vim.fn.setreg(vim.v.register, path)
+    end
+
+    -- Open path with system default handler (useful for non-text files)
+    local ui_open = function() vim.ui.open(mini_files.get_fs_entry().path) end
+
+    vim.api.nvim_create_autocmd('User', {
+        group = mini_files_group,
+        pattern = 'MiniFilesBufferCreate',
+        callback = function(args)
+            local b = args.data.buf_id
+            vim.keymap.set('n', 'g~', set_cwd,   { buffer = b, desc = 'Set cwd' })
+            vim.keymap.set('n', 'gX', ui_open,   { buffer = b, desc = 'OS open' })
+            vim.keymap.set('n', 'gy', yank_path, { buffer = b, desc = 'Yank path' })
+        end,
+    })
 end
 
 configs.mini_misc = function()
