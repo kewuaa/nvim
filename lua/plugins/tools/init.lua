f9e5185b77553304e209d9c67880d5d6a9060b96
kewuaa <202321011111@std.uestc.edu.cn>
2025-06-13 08:13:20 +0800

    add keymaps for open file explorer in cwd

--- lua/plugins/tools/init.lua
+++ lua/plugins/tools/init.lua
@@ -293,6 +293,9 @@ deps.add({
             {mode = "n", lhs = "<leader>fe", rhs = function()
                 require("mini.files").open()
             end},
+            {mode = "n", lhs = "<leader>fwd", rhs = function()
+                require("mini.files").open(vim.api.nvim_buf_get_name(0), false)
+            end}
         }
     },
     config = configs.mini_files
