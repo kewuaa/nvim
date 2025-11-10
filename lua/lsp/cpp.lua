0aed5910afc22481f314f0b231ee03726db59349
kewuaa <1692525710@qq.com>
2025-11-10 23:43:36 +0800

    update clangd options

--- lua/lsp/cpp.lua
+++ lua/lsp/cpp.lua
@@ -66,7 +66,7 @@ M.clangd = {
     cmd = {
         'clangd',
         "--offset-encoding=utf-16",
-        -- "--all-scopes-completion", -- 全局补全(补全建议会给出在当前作用域不可见的索引,插入后自动补充作用域标识符),例如在main()中直接写cout,即使没有`#include <iostream>`,也会给出`std::cout`的建议,配合"--header-insertion=iwyu",还可自动插入缺失的头文件
+        "--all-scopes-completion", -- 全局补全(补全建议会给出在当前作用域不可见的索引,插入后自动补充作用域标识符),例如在main()中直接写cout,即使没有`#include <iostream>`,也会给出`std::cout`的建议,配合"--header-insertion=iwyu",还可自动插入缺失的头文件
         "--background-index", -- 后台分析并保存索引文件
         "--clang-tidy", -- 启用 Clang-Tidy 以提供「静态检查」，下面设置 clang tidy 规则
         -- "--clang-tidy-checks=performance-*, bugprone-*, misc-*, google-*, modernize-*, readability-*, portability-*",
@@ -79,7 +79,7 @@ M.clangd = {
         "--fallback-style=google", -- 默认格式化风格: 在没找到 .clang-format 文件时采用,可用的有 LLVM, Google, Chromium, Mozilla, Webkit, Microsoft, GNU
         "--function-arg-placeholders=true", -- 补全函数时，将会给参数提供占位符，键入后按 Tab 可以切换到下一占位符，乃至函数末
         "--header-insertion-decorators", -- 输入建议中，已包含头文件的项与还未包含头文件的项会以圆点加以区分
-        "--header-insertion=never", -- 插入建议时自动引入头文件 iwyu
+        "--header-insertion=iwyu", -- 插入建议时自动引入头文件 设置为 never 时关闭
         -- "--include-cleaner-stdlib", -- 为标准库头文件启用清理功能(不成熟!!!)
         -- "--log=verbose", -- 让 Clangd 生成更详细的日志
         "--pch-storage=memory", -- pch 优化的位置(Memory 或 Disk,前者会增加内存开销，但会提升性能)
