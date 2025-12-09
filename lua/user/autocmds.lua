58b6015fab45b96dda87f968297bfc86ff8acd37
kewuaa <1692525710@qq.com>
2025-12-09 21:22:06 +0800

    add mini.cmdline

--- lua/user/autocmds.lua
+++ lua/user/autocmds.lua
@@ -50,43 +50,6 @@ local number_toggle = function()
     })
 end
 
-local cmd_completion = function()
-    vim.cmd([[set wildcharm=<C-@>]])
-    local term = vim.api.nvim_replace_termcodes('<C-@>', true, true, true)
-    local timer = vim.uv.new_timer()
-    local trigger_completion = vim.schedule_wrap(function()
-        local cmdline = vim.fn.getcmdline()
-        local curpos = vim.fn.getcmdpos()
-        local last_char = cmdline:sub(curpos - 1, curpos - 1)
-
-        if
-            curpos == #cmdline + 1
-            and vim.fn.pumvisible() == 0
-            and last_char:match('[%w%/%:- ]')
-            and not cmdline:match('^%d+$')
-            and not vim.tbl_isempty(vim.fn.getcompletion(cmdline, "cmdline"))
-        then
-            vim.api.nvim_feedkeys(term, 'ti', false)
-            vim.opt.eventignore:append('CmdlineChanged')
-            vim.schedule(function()
-                vim.fn.setcmdline(vim.fn.substitute(vim.fn.getcmdline(), '\\%x00', '', 'g'))
-                vim.opt.eventignore:remove('CmdlineChanged')
-            end)
-        end
-    end)
-
-    vim.api.nvim_create_autocmd('CmdlineChanged', {
-        desc = 'Auto show command line completion',
-        group = vim.api.nvim_create_augroup('autocomplete-cmd', {}),
-        pattern = ':',
-        callback = function()
-            assert(timer)
-            timer:stop()
-            timer:start(50, 0, trigger_completion)
-        end,
-    })
-end
-
 ---register filetypes to auto map 'q' to quit
 ---@param ... string
 M.register_quick_quit = function(...)
@@ -99,7 +62,6 @@ end
 M.init = function()
     quick_quit()
     number_toggle()
-    cmd_completion()
 end
 
 return M
