eed995eb8a8c9f59f475260d643bec571d618426
kewuaa <1692525710@qq.com>
2026-04-24 10:26:16 +0800

    load vim-suda on command `SudaRead` and `SudaWrite`

--- lua/plugins/configs/vim_suda.lua
+++ lua/plugins/configs/vim_suda.lua
@@ -1,1 +1,7 @@
+local utils = require("utils")
+
 vim.g["suda#prompt"] = "Enter administrator password: "
+
+utils.on_command({ "SudaWrite", "SudaRead" }, function()
+    vim.cmd.packadd("vim-suda")
+end)
