c49111cd5219dd41219825c269444407f78e5748
kewuaa <1692525710@qq.com>
2026-06-05 11:39:16 +0800

    use default

--- lua/plugins/configs/mini_notify.lua
+++ lua/plugins/configs/mini_notify.lua
@@ -2,8 +2,6 @@ local mini_notify = require("mini.notify")
 
 mini_notify.setup()
 
-vim.notify = mini_notify.make_notify({ ERROR = { duration = 10000 } })
-
 vim.api.nvim_create_user_command(
     "MiniNotifyHistory",
     mini_notify.show_history,
